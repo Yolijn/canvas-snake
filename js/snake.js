664d4b6498a7c87317b1c1295b72b83597eab11d
Yolijn <mail@yolijn.com>
2016-04-26 16:39:18 +0200

    Default styling, random blocks and snake

--- js/snake.js
+++ js/snake.js
@@ -1,13 +1,29 @@
 function start() {
 	var canvas = document.getElementById("canvas");
 	var context = canvas.getContext("2d");
-	var posX = Math.floor((Math.random() * 298));
-	console.log(posX);
-	var posY = Math.floor((Math.random() * 149));;
-	console.log(posY);
-	context.fillStyle = "black";
-	// (position x, position y, width, height)
-	context.fillRect(posX, posY, 2, 1);
+	var maxX = 298;
+	var maxY = 149;
+	var blockWidth = 4;
+	var blockHeight = 2;
+	var randomBlocks = 10;
+	var snakeLength = 10;
+
+	context.fillStyle = "#424242";
+
+	// Create random number between 0 and maxX or maxY for random positioning
+	function randomPos(max){
+		var position = Math.floor(Math.random() * max);
+		return position;
+	}
+
+	
+	// Fill canvas with random blocks
+	for (i = 1; i <= randomBlocks; i++){
+		context.fillRect(randomPos(maxX), randomPos(maxY), blockWidth, blockHeight);	
+	}
+
+	// Create snake at start position
+	context.fillRect(2, 1, blockWidth * snakeLength, blockHeight)
 }
 
 window.onload = start;
